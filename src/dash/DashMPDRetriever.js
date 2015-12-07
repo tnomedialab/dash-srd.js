49c639ac9c1129c0dcf11ab5711f7dcdcdd4a3f8
jberg <jorrit.vandenberg@tno.nl>
2015-12-07 15:35:55 +0100

    Created XML retrieval script

---
+++ src/dash/DashMPDRetriever.js
@@ -0,0 +1,57 @@
+/* 
+ * Created bij Jorrit van den Berg on 07/12/15.
+ * Copyright (c) 2015, TNO.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+// Function to perform MPD retrieval
+// Returns MPD (XML) file in case of succes
+
+function getMPD (var url) {
+    "use strict";
+    
+    // Assign variables and perform XMLHttpRequest
+    var mpdFile = null;
+    var xhttp = new XMLHttpRequest();
+    
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            var mpdFile = xhttp.responseXML;
+            
+        }
+        else {
+            // Undefined error case
+            log("Connection error");
+        }
+        
+    };
+    
+    xhttp.open("GET", url, true);
+    xhttp.send();
+    
+    // Return MPD file if retrieved (defaults to null if failed)
+    return mpdFile;
+    
+}
+    
